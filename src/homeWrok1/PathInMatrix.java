79a3f4613df71900326604239cdc34084ef00386
Krachunov <Krachunov@Krachunov-PC>
2015-09-23 16:04:38 +0300

    Print path labiyrinth

--- src/homeWrok1/PathInMatrix.java
+++ src/homeWrok1/PathInMatrix.java
@@ -1,9 +1,20 @@
 package homeWrok1;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PathInMatrix {
+	static List<Character> dir = new ArrayList<Character>();
+
+	static void printDir() {
+		
+		for (Character character : dir) {
+			System.out.print(character);
+		}
+	}
 
 	public static void findPath(char[][] matrix, int x, int y, char symbol) {
-		 int count = 0;
+		int count = 0;
 		int row = x;
 		int col = y;
 		if (row < 0 || col < 0 || row >= matrix.length
@@ -13,6 +24,8 @@ public class PathInMatrix {
 
 		if (matrix[row][col] == 'e') {
 
+			printDir();
+			System.out.println();
 			System.out.println("Exit");
 			return;
 		}
@@ -22,13 +35,14 @@ public class PathInMatrix {
 
 		System.out.print(symbol);
 		matrix[row][col] = 'x';
-
+		dir.add(symbol);
 		findPath(matrix, x, y + 1, 'R');
 		findPath(matrix, x + 1, y, 'D');
 		findPath(matrix, x, y - 1, 'L');
 		findPath(matrix, x - 1, y, 'U');
 
 		matrix[row][col] = ' ';
+		dir.remove(dir.size() - 1);
 	}
 
 	public static void main(String[] args) {

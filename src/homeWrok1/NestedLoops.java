292b99815dbf35663bf12c94410f645f5e8c0fd4
Krachunov <Krachunov@Krachunov-PC>
2015-09-23 18:08:04 +0300

    Nested Loops

--- src/homeWrok1/NestedLoops.java
+++ src/homeWrok1/NestedLoops.java
@@ -1,26 +1,31 @@
 package homeWrok1;
 
 public class NestedLoops {
-	static int c = 0;
-	static int[] arr;
-	static int index = 0;
 
-	public static void nestetLoop(int index, int[] arr) {
-		if (index < 0) {
-			for (int i : arr) {
-				System.out.println(i);
-			}
-		} else {
-			for (int i = 0; i < index; i++) {
-				arr[i]=i;
-				nestetLoop(index-1,arr);
-			}
+	static int[] loops;
+	static int n = 3;
+
+	public static void nestedLoops(int currentLoop) {
+		if (currentLoop == n) {
+			printLoops();
+			return;
+		}
+		for (int i = 1; i <= n; i++) {
+			loops[currentLoop] = i;
+			nestedLoops(currentLoop + 1);
 		}
 	}
 
+	public static void printLoops() {
+		for (int i = 0; i < n; i++) {
+			System.out.printf("%d ", loops[i]);
+		}
+		System.out.println();
+	}
+
 	public static void main(String[] args) {
-		int n = 3;
-		arr = new int[n];
-		nestetLoop(n, arr);
+
+		loops = new int[n];
+		nestedLoops(0);
 	}
 }

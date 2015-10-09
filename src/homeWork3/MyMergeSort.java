1f30dfe225b34423df076f9c41bc83cd844672d7
Krachunov <Krachunov@Krachunov-PC>
2015-10-09 17:14:45 +0300

    problem3

--- src/homeWork3/MyMergeSort.java
+++ src/homeWork3/MyMergeSort.java
@@ -3,20 +3,55 @@ package homeWork3;
 import java.util.Arrays;
 
 public class MyMergeSort {
-	public static void mertgeSort(int[] arr, int start, int end) {
+	public static void mertgeSort(int[] arr) {
+		for (int i : arr) {
+			System.out.print(i + " ");
+		}
+		System.out.println();
+
+		int start = 0;
+		int end = arr.length;
+		int mid = (start + end) / 2;
+		int[] firstArr = new int[mid];
+		int[] secondtArr = new int[end - mid];
+		int[] sortedArr = new int[firstArr.length + secondtArr.length];
 
 		if (arr.length > 1) {
-			int mid = (start + end) / 2;
-			mertgeSort(Arrays.copyOfRange(arr, start, mid), start, mid);
-			mertgeSort(Arrays.copyOfRange(arr, mid + 1, end), mid + 1, end);
+			firstArr = Arrays.copyOfRange(arr, start, mid);
+			secondtArr = Arrays.copyOfRange(arr, mid, end);
+			mertgeSort(firstArr);
+			mertgeSort(secondtArr);
 		} else {
+			int leftIndex = 0;
+			int rigthIndex = 0;
+			
+			for (int i = 0; i < sortedArr.length; i++) {
+				if (leftIndex < firstArr.length
+						&& rigthIndex < secondtArr.length) {
+					if (firstArr[leftIndex] <= secondtArr[rigthIndex]) {
+						sortedArr[i] = firstArr[leftIndex];
+						leftIndex++;
+					} else {
+						sortedArr[i] = secondtArr[rigthIndex];
+						rigthIndex++;
+					}
+				} else if (leftIndex < firstArr.length&& rigthIndex > secondtArr.length) {
+					sortedArr[i] = firstArr[leftIndex];
+					leftIndex++;
+				} else if (leftIndex > firstArr.length&& rigthIndex < secondtArr.length)  {
+					sortedArr[i] = secondtArr[rigthIndex];
+					rigthIndex++;
+				}else{
+					return;
+				}
+			}
 			return;
 		}
 	}
 
 	public static void main(String[] args) {
-		int[] f = { 2, 3, 4, 1, 5, 1, 1, 5 };
-		mertgeSort(f, 0, f.length);
+		int[] f = { 38, 27, 43, 3, 9, 82, 10 };
+//		int[] a = new int[f.length];
+		mertgeSort(f);
 	}
-
 }

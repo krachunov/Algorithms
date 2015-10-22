5d307ca7d90ad2c282ac2143c9b5e58d6a7372a8
Krachunov <Krachunov@Krachunov-PC>
2015-10-22 17:30:42 +0300

    Problem1

--- src/homeWork5/DistanceBetweenVertices.java
+++ src/homeWork5/DistanceBetweenVertices.java
@@ -6,17 +6,30 @@ import java.util.Scanner;
 
 public class DistanceBetweenVertices {
 
-	static void adjacencyLists(List list, int num) {
+	static void adjacencyLists(List<List<Integer>> list, int num) {
+		Scanner sc = new Scanner(System.in);
 
-		for (int i = 0; i < num; i++)
-			list.set(i, new ArrayList<Integer>());
+		for (int i = 0; i < num; i++) {
+			list.add(new ArrayList<Integer>());
+		}
+		System.out.println("enter the parent");
+		int parent = sc.nextInt();
+		System.out.println("enter the child");
+		int child = sc.nextInt();
+		addEdge(list, parent, child);
+
+	}
+
+	static void addEdge(List list, int i, int j) {
+		List node = (List) list.get(i);
+		node.add(j);
 	}
 
 	public static void main(String[] args) {
 
-		int numberOfNode = 5;
+		int numberOfNode = 2;
 		List<List<Integer>> graph = new ArrayList<List<Integer>>();
 		adjacencyLists(graph, numberOfNode);
-		
+
 	}
 }

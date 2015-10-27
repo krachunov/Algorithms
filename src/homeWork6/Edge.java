cee0c79747f682cc15bf2623844d3562f72a7b32
Krachunov <Krachunov@Krachunov-PC>
2015-10-27 16:45:13 +0200

    Start homeWork6

---
+++ src/homeWork6/Edge.java
@@ -0,0 +1,70 @@
+package homeWork6;
+
+public class Edge<T> implements Comparable<Edge> {
+	private T startNode;
+	private T endNode;
+	private int wightl;
+	private boolean connected;
+
+	public Edge(T startNode, T endNode, int wightl, String connected) {
+		setStartNode(startNode);
+		setEndNode(endNode);
+		setWightl(wightl);
+		if (connected.contains(connected)) {
+			setConnected(true);
+		}
+	}
+
+	public Edge(T startNode, T endNode, int wightl) {
+		setStartNode(startNode);
+		setEndNode(endNode);
+		setWightl(wightl);
+		setConnected(false);
+	}
+
+	public T getStartNode() {
+		return startNode;
+	}
+
+	public void setStartNode(T startNode) {
+		this.startNode = startNode;
+	}
+
+	public T getEndNode() {
+		return endNode;
+	}
+
+	public void setEndNode(T endNode) {
+		this.endNode = endNode;
+	}
+
+	public int getWightl() {
+		return wightl;
+	}
+
+	public void setWightl(int wightl) {
+		this.wightl = wightl;
+	}
+
+	public boolean isConnected() {
+		return connected;
+	}
+
+	public void setConnected(boolean connected) {
+		this.connected = connected;
+	}
+
+	@Override
+	public int compareTo(Edge other) {
+		int wightCompared = (this.wightl > other.wightl ? 1
+				: this.wightl < other.wightl ? -1 : 0);
+		return wightCompared;
+	}
+
+	@Override
+	public String toString() {
+		return "(" + this.getStartNode() + " " + this.getEndNode() + ")-> "
+				+ this.getWightl();
+
+	}
+}
---
+++ src/homeWork6/ExtendCableNetwork.java
@@ -0,0 +1,18 @@
+/**
+ * Problem 1. Extend a Cable Network
+A cable networking company plans to extend its existing cable network by connecting as many customers as possible within a fixed budget limit.
+The company has calculated the cost of building some prospective connections.
+You are given the existing cable network (a set of customers and connections between them)
+along with the estimated connection costs between some pairs of customers and prospective customers. Example:
+ */
+package homeWork6;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ExtendCableNetwork {
+	public static void main(String[] args) {
+		List<Edge>edges = new ArrayList<Edge>();
+		edges.add(new Edge<String>("A","B",3));
+	}
+}

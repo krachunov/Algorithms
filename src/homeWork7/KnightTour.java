016849c49bb9756f8a64f9076c061eb641b743d1
Krachunov <Krachunov@Krachunov-PC>
2015-11-03 17:19:55 +0200

    Start Problem 3

---
+++ src/homeWork7/KnightTour.java
@@ -0,0 +1,53 @@
+/**
+ * Problem 3. Knight’s Tour
+Given a board of size NxN (a standard square matrix), a chess knight can perform a tour of the board, visiting each cell only once.
+The knight moves according to the rules of chess (in an L-shaped pattern) and starts from the upper-left corner.
+Write a program which finds and prints the path the knight needs to take in order to visit all cells.
+From the input you receive just the board size N (N > 4). On the output, print the board where each cell’s value is the number of the step which led to it,
+e.g. cell (0, 0) will be 1, the first step, the next visited cell (0, 2) will have value 2, etc. In order to format the output,
+ assume NxN < 1000 – no cell’s value will be more than 3 digits (you can pad the values with spaces with the string.PadLeft() method).
+ */
+
+package homeWork7;
+
+import java.util.List;
+
+public class KnightTour {
+
+	public static void findPath(int[][] matrix, int row, int col, int symbol) {
+		int snakeLength = 0;
+		// check if out of array
+		if (row < 0 || col < 0 || row >= matrix.length
+				|| col >= matrix[row].length) {
+			return;
+		}
+		// check is free
+		if (matrix[row][col] != ' ') {
+			return;
+		}
+
+		if (snakeLength > 0) {
+			snakeLength--;
+			matrix[row][col] = 'x';
+
+			findPath(matrix, row - 1, col - 2, 'R');
+			findPath(matrix, row - 1, col + 2, 'D');
+			findPath(matrix, row + 1, col + 2, 'D');
+			findPath(matrix, row + 1, col - 2, 'D');
+			findPath(matrix, row - 2, col - 1, 'D');
+			findPath(matrix, row - 2, col + 1, 'D');
+			findPath(matrix, row + 2, col + 1, 'D');
+			findPath(matrix, row + 2, col - 1, 'D');
+
+			snakeLength++;
+
+			matrix[row][col] = ' ';
+
+		}
+	}
+
+	public static void knightTour(int n) {
+		int[][] board = new int[n][n];
+
+	}
+}
